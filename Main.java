c753899f92e8024c36469bc00f3ee49ca4f97345
Pouyan <151916970+yan-jordan@users.noreply.github.com>
2024-06-29 17:34:17 +0330

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,8 @@
+import java.io.IOException;
+import java.sql.*;
+public class Main {
+    public static void main(String[] args) throws IOException, SQLException {
+        new Graphic();
+
+    }
+}
