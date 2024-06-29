e8b3a9c54f3543b4dd11149b691407a49c8e5f93
Pouyan <151916970+yan-jordan@users.noreply.github.com>
2024-06-29 17:39:22 +0330

    Add files via upload

---
+++ Manager.java
@@ -0,0 +1,18 @@
+public class Manager {
+private String employeeCode;
+private String employeePassword;
+    public String getEmployeeCode() {
+        return employeeCode;
+    }
+    public void setEmployeeCode(String employeeCode) {
+        this.employeeCode = employeeCode;
+    }
+
+    public String getEmployeePassword() {
+        return employeePassword;
+    }
+    public void setEmployeePassword(String employeePassword) {
+        this.employeePassword = employeePassword;
+    }
+}
+
